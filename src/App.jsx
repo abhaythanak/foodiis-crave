fe3251b4b51dca3c844184da10da9e9e946a1f1e
Abhay Thanak <abhaythanak@gmail.com>
2023-12-11 15:04:38 +0530

    1. components created Banner, AppStore, and services 2.in services some example products added 3. banner file images and foodieess looks things and text added it will change the good descreaptions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import Navbar from "./components/Navbar/Navbar"
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Services from "./components/Services/Services.jsx";
+import Banner from "./components/Banner/Banner.jsx";
+import AppStore from "./components/AppStore/AppStore.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
-function App() {
+
+const App = () => {
 
   return (
-    <>
-      <Navbar/>
-    </>
-  )
-}
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <Navbar />
+      <Services />
+      <Banner />
+      <AppStore />
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
